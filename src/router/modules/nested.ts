8bb9f5396ec07a8f5bda423a1d975b6a17594878
wangyang0210 <i@easybe.org>
2023-04-11 00:06:25 +0800

    chore: system menu

--- src/router/modules/nested.ts
+++ src/router/modules/nested.ts
@@ -16,58 +16,7 @@ export default {
         title: $t("menus.hsmenu1"),
         keepAlive: true
       },
-      redirect: "/nested/menu1/menu1-1",
-      children: [
-        {
-          path: "/nested/menu1/menu1-1",
-          component: () => import("@/views/nested/menu1/menu1-1/index.vue"),
-          name: "Menu1-1",
-          meta: {
-            title: $t("menus.hsmenu1-1"),
-            keepAlive: true
-          }
-        },
-        {
-          path: "/nested/menu1/menu1-2",
-          redirect: "/nested/menu1/menu1-2/menu1-2-1",
-          meta: {
-            title: $t("menus.hsmenu1-2"),
-            keepAlive: true
-          },
-          children: [
-            {
-              path: "/nested/menu1/menu1-2/menu1-2-1",
-              component: () =>
-                import("@/views/nested/menu1/menu1-2/menu1-2-1/index.vue"),
-              name: "Menu1-2-1",
-              meta: {
-                title: $t("menus.hsmenu1-2-1"),
-                keepAlive: true
-              }
-            },
-            {
-              path: "/nested/menu1/menu1-2/menu1-2-2",
-              component: () =>
-                import("@/views/nested/menu1/menu1-2/menu1-2-2/index.vue"),
-              name: "Menu1-2-2",
-              meta: {
-                title: $t("menus.hsmenu1-2-2"),
-                keepAlive: true,
-                extraIcon: "IF-pure-iconfont-new svg"
-              }
-            }
-          ]
-        },
-        {
-          path: "/nested/menu1/menu1-3",
-          component: () => import("@/views/nested/menu1/menu1-3/index.vue"),
-          name: "Menu1-3",
-          meta: {
-            title: $t("menus.hsmenu1-3"),
-            keepAlive: true
-          }
-        }
-      ]
+      redirect: "/nested/menu1/menu1-1"
     },
     {
       path: "/nested/menu2",
