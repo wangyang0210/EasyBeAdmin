a2542a5926d42f56f873e0c13552147c84e49c21
wangyang0210 <i@easybe.org>
2023-04-11 01:22:03 +0800

    chore(menu): user center

--- src/router/enums.ts
+++ src/router/enums.ts
@@ -1,8 +1,9 @@
 // 完整版菜单比较多，将 rank 抽离出来，在此方便维护
 
 const home = 0, // 平台规定只有 home 路由的 rank 才能为 0 ，所以后端在返回 rank 的时候需要从 1 开始哦
-  data = 1,
-  system = 2;
+  user = 1,
+  data = 2,
+  system = 3;
 // doc = 1,
 // utils = 2,
 // table = 3,
@@ -25,25 +26,26 @@ const home = 0, // 平台规定只有 home 路由的 rank 才能为 0 ，所以�
 
 export {
   home,
+  user,
   data,
-  system,
-  doc,
-  utils,
-  table,
-  components,
-  able,
-  frame,
-  nested,
-  result,
-  error,
-  list,
-  permission,
-  tabs,
-  formdesign,
-  flowchart,
-  ppt,
-  editor,
-  guide,
-  menuoverflow,
-  about
+  system
+  // doc,
+  // utils,
+  // table,
+  // components,
+  // able,
+  // frame,
+  // nested,
+  // result,
+  // error,
+  // list,
+  // permission,
+  // tabs,
+  // formdesign,
+  // flowchart,
+  // ppt,
+  // editor,
+  // guide,
+  // menuoverflow,
+  // about
 };
---
+++ src/router/modules/user.ts
@@ -0,0 +1,31 @@
+import { $t } from "@/plugins/i18n";
+import { user } from "@/router/enums";
+
+export default {
+  path: "/user",
+  redirect: "/nested/menu1/menu1-1",
+  meta: {
+    title: $t("menus.hsUserCenter"),
+    icon: "histogram",
+    rank: user
+  },
+  children: [
+    {
+      path: "/nested/menu1",
+      meta: {
+        title: $t("menus.hsUser"),
+        keepAlive: true
+      },
+      redirect: "/nested/menu1/menu1-1"
+    },
+    {
+      path: "/nested/menu2",
+      name: "Menu2",
+      component: () => import("@/views/nested/menu2/index.vue"),
+      meta: {
+        title: $t("menus.hsRole"),
+        keepAlive: true
+      }
+    }
+  ]
+} as RouteConfigsTable;
